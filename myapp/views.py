efeb5a659bb141e07503d8b50cc66775dd17d18a
matwiniarczyk <matiwiniar@gmail.com>
2024-07-22 13:02:12 +0200

    project views

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,37 @@
+from django.db.models import Sum
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+from myapp.models import Donation, Institution
+
+
+class LandingPageView(View):
+    def get(self, request):
+        return render(request, 'base.html')
+
+
+class AddDonationView(View):
+    def get(self, request):
+        return render(request, 'form.html')
+
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, 'login.html')
+
+
+class RegisterView(View):
+    def get(self, request):
+        return render(request, 'register.html')
+
+
+class IndexView(View):
+    def get(self, request):
+        bags = Donation.objects.aggregate(total_bags=Sum('quantity'))
+        # aggregate zwraca słownik, klucze nazywają się 'total bags'
+        total_bags = bags['total_bags']
+        institutions = Donation.objects.values('institution').distinct()
+        # values zwraca listę słowników klucz-wartość; tutaj klucz to institution
+        total_institutions = institutions.count()
+        return render(request, 'index.html', {'liczba_worków': total_bags, 'liczba_instytucji': total_institutions})
+
