cbf8e34b065f096f89745d48600ee14999a468d8
matwiniarczyk <matiwiniar@gmail.com>
2024-07-22 13:01:00 +0200

    urls created

--- Portfolio_lab_project/urls.py
+++ Portfolio_lab_project/urls.py
@@ -18,6 +18,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from myapp import views
+from myapp.views import LandingPageView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.LandingPageView.as_view(), name='landing_page'),
+    path('add_donation/', views.AddDonationView.as_view(), name='add_donation'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('register/', views.RegisterView.as_view(), name='register'),
+    path('index/', views.IndexView.as_view(), name='index'),
 ]
